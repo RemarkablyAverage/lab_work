19f9b408b9fa985e2724a6689cd802bcfde3b7b6
Daniel <li.daniel@berkeley.edu>
2016-03-25 02:10:04 -0700

    back at it with those white shoes

--- distributions.R
+++ distributions.R
@@ -75,8 +75,8 @@ create_transcripts <- function(
 	for (x in 1:transcript_number) {
 		individual_transcript <- c()
 		if (variation) {
-			start <- as.integer(length - sample(length/10:length, 1))
-			end <- as.integer(length + sample(length:length*1.1, 1))
+			start <- as.integer(length - sample(length/8:length, 1))
+			end <- as.integer(length + sample(length:length*1.2, 1))
 			length_loop <- sample(start:end, 1)
 			print(length_loop)
 		} else {
@@ -84,7 +84,7 @@ create_transcripts <- function(
 		}
 		for (i in 1:length_loop) {
 			rand <- as.integer(sample((1:end_sample), 1))
-			individual_transcript <- paste(individual_transcript, (chop_df[rand,]), sep = "")
+			individual_transcript <- paste(individual_transcript, (chop_df[rand,]), sep = "|")
 		}
 		transcripts <- c(transcripts, individual_transcript)
 	}
@@ -92,10 +92,38 @@ create_transcripts <- function(
 	ret_df
 }
 
+#plot transcripts as a function of y = e^-x and 
+#use | as delminator
+#use nchar as count
 
-opstr <- generate_string(100000)
-chopped_up <- create_cuts(operating_str=opstr, filterby=100, cuts=1000)
-transcripts <- create_transcripts(chopped_up)
+plot_transcripts <- function(
+	dataf = NULL,
+	set_distribution_length = NULL,
+	distribution_type = NULL) {
+
+	#parse string
+	distribution_vector <- c()
+
+	for (t in 1:nrow(dataf)) {
+		#iterate through all transcripts
+		transcript <- dataf[t,]
+		for (i in 1:nchar(transcript)) {
+
+		}
+		#call plot function
+		plot(transcript, distribution_type)
+	}
+}
+
+plot <- function(
+	transcript = NULL,
+	distribution_type = NULL) {
+
+}
+
+opstr <- generate_string(1000)
+chopped_up <- create_cuts(operating_str=opstr, filterby=100, cuts=15)
+transcripts <- create_transcripts(chopped_up, length=10)
 
 
 
